ab28e8be77322b92f23715bac5c4f9a845cf433d
Adham El Banhawy <the.benhawy@gmail.com>
2020-12-24 00:51:57 +0200

    Added desired & offered price inputs & state vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
+import Input from '@material-ui/core/Input';
 
 const theme = createMuiTheme({
   typography: {
-    fontFamily: [ 'Balsamiq Sans', 'Roboto', 'sans-serif' ].join(',')
+    fontFamily: ['Balsamiq Sans', 'Roboto', 'sans-serif'].join(',')
   }
 })
 
 function App() {
+  const [desiredPrice, setDesiredPrice] = useState(0);
+  const [offeredPrice, setOfferedPrice] = useState(0);
+
+  const onDesiredChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setDesiredPrice(Number(event.target.value))
+  }
+
+  const onOfferedChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setOfferedPrice(Number(event.target.value))
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <div className="App" style={{textAlign: 'center'}}>
-        <Typography variant="h2">Price Negotiator</Typography> 
+      <div className="App" style={{ textAlign: 'center' }}>
+        <Typography variant="h2">Price Negotiator</Typography>
+
+        Enter Desired Price
+        <Input
+          type="number"
+          inputProps={{ min: "0" }}
+          onChange={onDesiredChange}></Input>
+
+        <br />
+        <br />
+
+        Enter Offered Price
+        <Input
+          type="number"
+          inputProps={{ min: "0" }}
+          onChange={onOfferedChange}></Input>
       </div>
     </ThemeProvider>
   );
