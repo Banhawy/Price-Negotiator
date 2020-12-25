2f1b24440cd6ed3dc6e7379658b659d6b0a83612
Adham El Banhawy <the.benhawy@gmail.com>
2020-12-25 20:56:37 +0200

    returned abs value for percentage difference

--- src/components/PriceDifference.tsx
+++ src/components/PriceDifference.tsx
@@ -6,21 +6,21 @@ interface PriceDifferenceProps {
 }
 
 function calculateDifference(firstNumber: number, secondNumber: number) {
-    const numberValue: number = firstNumber - secondNumber;
-    const absNumberValue : number = Math.abs(numberValue)
-    const percentageValue: number = Number(((numberValue / secondNumber) * 100).toFixed(2))
-    const isPositive: boolean = numberValue >= 0 ? true : false;
+    const absNumberValue : (number: number) => number = (value) => Math.abs(value)
+    const numberValue: number = absNumberValue(firstNumber - secondNumber);
+    const percentageValue: number = absNumberValue(Number(((numberValue / secondNumber) * 100).toFixed(2)))
+    const isPositive: boolean = firstNumber - secondNumber >= 0 ? true : false;
 
-    return { percentageValue, absNumberValue, isPositive }
+    return { percentageValue, numberValue, isPositive }
 }
 
 export default function PriceDifference(props: PriceDifferenceProps) {
     const { desiredPrice, offeredPrice } = props
-    const { percentageValue, absNumberValue, isPositive } = calculateDifference(offeredPrice, desiredPrice)
+    const { percentageValue, numberValue, isPositive } = calculateDifference(offeredPrice, desiredPrice)
 
     return (
         <div>
-            The offered price is {percentageValue}% - {absNumberValue} <strong>{isPositive ? 'MORE ' : 'LESS '}</strong> than your desired price.
+            The offered price is {percentageValue}% - {numberValue} <strong>{isPositive ? 'MORE ' : 'LESS '}</strong> than your desired price.
         </div>
     )
 }
