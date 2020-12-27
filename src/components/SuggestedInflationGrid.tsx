f5684537b2ecb3ca03f9c4f66dc3ae7286011939
Adham El Banhawy <the.benhawy@gmail.com>
2020-12-27 19:15:27 +0200

    dynamically decide suggested inflations

--- src/components/SuggestedInflationGrid.tsx
+++ src/components/SuggestedInflationGrid.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Grid from '@material-ui/core/Grid'
 import SuggestedInflation from './SuggestedInflation'
-
+import PriceContext from '../PriceContext'
+import { suggestedInflations } from "./utils"
 
 
 export default function SuggestedInflationGrid() {
+    const { desiredPrice, offeredPrice } = useContext(PriceContext)
+    const suggestedPercentages = suggestedInflations(desiredPrice, offeredPrice)
     return (
-        <Grid container spacing={3} style={{justifyContent: 'center', flexWrap: 'wrap', marginTop: '2rem'}}>
-            <Grid item md={3} sm={3} xs={12}>
-                <SuggestedInflation percentage={10}/>
-            </Grid>
-            <Grid item md={3} sm={3} xs={12}>
-                <SuggestedInflation percentage={15}/>
-            </Grid>
-            <Grid item md={3} sm={3} xs={12}>
-                <SuggestedInflation percentage={20}/>
-            </Grid>
+        <Grid container spacing={3} style={{ justifyContent: 'center', flexWrap: 'wrap', marginTop: '2rem' }}>
+            {
+                suggestedPercentages.map((percentage, index) => (
+                    <Grid key={index} item md={3} sm={3} xs={12}>
+                        <SuggestedInflation percentage={percentage} />
+                    </Grid>
+                ))
+            }
         </Grid>
     )
 }
--- src/components/utils.ts
+++ src/components/utils.ts
@@ -5,13 +5,14 @@ interface DifferenceResults {
     percentageValue: number,
     /**
      * The difference as an integer value
-     */ 
-    numberValue: number, 
+     */
+    numberValue: number,
     /**
      * a flag to indicate if difference is positive or negative
      */
-    isPositive: boolean 
+    isPositive: boolean
 }
+
 /**
  * A function that calculates the difference between two numbers ammd returns an object cont
  * @param {number} firstNumber 
@@ -26,4 +27,32 @@ export function calculateDifference(firstNumber: number, secondNumber: number): 
     const isPositive: boolean = firstNumber - secondNumber >= 0 ? true : false;
 
     return { percentageValue, numberValue, isPositive }
+}
+
+/**
+ * A function that returns suggested inflation positive percentages based on current desired & offered prices
+ * @param desiredPrice the desired price entered by the user
+ * @param offeredPrice the offered price entered by the user
+ * @param increments the incremental increase in suggested inflations value
+ * @param count the number of suggested inflations to return
+ */
+export function suggestedInflations(desiredPrice: number, offeredPrice: number, increments = 5, count = 3) {
+    const { percentageValue, isPositive } = calculateDifference(offeredPrice, desiredPrice)
+    let minPerentageIncrease: number = 10; // This is the default if difference is positive
+    let suggestionArray = new Array<number>(count)
+
+    const isModFive = (num: number) => num % 5 === 0
+    const roundToFive = (num: number) => num + (5 - (num % 5))
+
+    if (!isPositive) {
+        minPerentageIncrease = isModFive(percentageValue) ? percentageValue + 5 : roundToFive(percentageValue)
+    }
+
+    let counter = 1;
+    for (let i = 0; i < suggestionArray.length; i++) {
+        suggestionArray[i] = minPerentageIncrease + counter * increments
+        counter++
+    }
+
+    return suggestionArray
 }
